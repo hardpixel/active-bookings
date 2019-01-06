99c307abb6c623bab3d4baf602a1d33282c2e7f6
Jonian Guveli <jonian@hardpixel.eu>
2019-01-06 13:06:09 +0200

    code style optimizations

--- lib/active_bookings.rb
+++ lib/active_bookings.rb
@@ -1,9 +1,10 @@
+require 'ice_cube'
 require 'active_record'
 require 'active_record/version'
 require 'active_support/core_ext/module'
-require 'ice_cube'
 
-IceCube.compatibility = 12 # Drop compatibility for :start_date, avoiding a bunch of warnings caused by serialization
+# Drop compatibility for :start_date, avoiding a bunch of warnings caused by serialization
+IceCube.compatibility = 12
 
 module ActiveBookings
   extend ActiveSupport::Autoload
@@ -22,13 +23,13 @@ module ActiveBookings
 
   class InitializationError < StandardError
     def initialize model, message
-      super "Error initializing active_bookings on #{model.to_s} - " + message
+      super "Error initializing active_bookings on #{model.to_s} - #{message}"
     end
   end
 
   class OptionsInvalid < StandardError
     def initialize model, message
-      super "Error validating options for #{model.to_s} - " + message
+      super "Error validating options for #{model.to_s} - #{message}"
     end
   end
 
